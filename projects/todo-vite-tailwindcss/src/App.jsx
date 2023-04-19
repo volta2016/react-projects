81fa28413ecb64cddac3f3f57009747801353e5d
volta2016 <giovoltadesign@gmail.com>
2023-04-18 22:25:39 -0400

    feat(UI): :lipstick: semantic Todo with tailwind v1

--- projects/todo-vite-tailwindcss/src/App.jsx
+++ projects/todo-vite-tailwindcss/src/App.jsx
@@ -3,17 +3,27 @@ import React from "react";
 const App = () => {
   return (
     <div
-      className="bg-[url('./assets/images/bg-mobile-light.jpg')] bg-contain 
+      className="min-h-screen bg-gray-200 bg-[url('./assets/images/bg-mobile-light.jpg')] bg-contain 
     bg-center bg-no-repeat"
     >
-      <header className="container mx-auto px-4">
-        <h1 className="uppercase">Todo</h1>
-        <button>luna</button>
-        <form>
-          <input type="text" placeholder="Create new Todo..." />
+      <header className="container mx-auto px-4 pt-8">
+        <div className="flex justify-between">
+          <h1 className="text-2xl uppercase tracking-[0.3em] text-white">
+            Todo
+          </h1>
+          <button>luna</button>
+        </div>
+
+        <form className="mt-8 flex items-center  gap-4 overflow-hidden rounded-md bg-white px-4 py-4">
+          <span className="inline-block  h-5 w-5  gap-4 rounded-full border-2"></span>
+          <input
+            className="w-full text-gray-400 outline-none"
+            type="text"
+            placeholder="Create new Todo..."
+          />
         </form>
       </header>
-      <main className="container mx-auto px-4">
+      <main className="container mx-auto mt-8 px-4">
         <article>
           <button>circle</button>
           <p>complete online JS course Front end mentor</p>
