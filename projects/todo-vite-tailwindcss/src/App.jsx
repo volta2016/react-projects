9152520f2bdb5d61b5674aaef21b66c58e422b99
volta2016 <giovoltadesign@gmail.com>
2023-05-01 17:18:17 -0400

    feat(UI): :lipstick: createTodo props to TodoCreate

--- projects/todo-vite-tailwindcss/src/App.jsx
+++ projects/todo-vite-tailwindcss/src/App.jsx
@@ -24,13 +24,13 @@ const initialStateTodos = [
   },
 
   {
-    id: 3,
+    id: 4,
     title: "Pick up Groceries",
     completed: false,
   },
 
   {
-    id: 3,
+    id: 5,
     title: "Complete Web App on frontend mentor",
     completed: false,
   },
@@ -39,11 +39,25 @@ const initialStateTodos = [
 const App = () => {
   const [todos, setTodos] = useState(initialStateTodos);
 
+  function generateID() {
+    return Math.random().toString(36).slice(2);
+  }
+
+  const createTodo = (title) => {
+    const newTodo = {
+      id: Date.now(),
+      title,
+      completed: false,
+    };
+
+    setTodos([...todos, newTodo]);
+  };
+
   return (
     <div className="min-h-screen bg-gray-200 bg-[url('./assets/images/bg-mobile-light.jpg')] bg-contain bg-no-repeat">
       <Header />
       <main className="container mx-auto mt-8 px-4">
-        <TodoCreate />
+        <TodoCreate createTodo={createTodo} />
 
         <Todolist todos={todos} />
 
--- projects/todo-vite-tailwindcss/src/components/TodoCreate.jsx
+++ projects/todo-vite-tailwindcss/src/components/TodoCreate.jsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
-const TodoCreate = () => {
+const TodoCreate = ({ createTodo }) => {
+  //The State state corresponds to the follow-up of the input which is a text
+  const [title, setTitle] = useState("");
+
+  const handleSubmitAddTodo = (e) => {
+    e.preventDefault();
+    if (!title.trim()) {
+      return setTitle("");
+    }
+
+    createTodo(title);
+    setTitle("");
+  };
+
   return (
-    <form className="mt-8 flex items-center  gap-4 overflow-hidden rounded-md bg-white px-4 py-4">
+    <form
+      onSubmit={handleSubmitAddTodo}
+      className="mt-8 flex items-center  gap-4 overflow-hidden rounded-md bg-white px-4 py-4"
+    >
       <span className="inline-block  h-5 w-5  gap-4 rounded-full border-2"></span>
       <input
         className="w-full text-gray-400 outline-none"
         type="text"
         placeholder="Create new Todo..."
+        title={title}
+        onChange={(e) => setTitle(e.target.value)}
       />
     </form>
   );
